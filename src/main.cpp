9c723d4575c6c6e1bbc215e35785e20939d4e71d
op3nwoundssw3 <roflantieblan@mail.ru>
2026-03-29 23:55:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
+#include <cmath>
+#include <iostream>
 #include "fun.h"
+#include <cmath>
 
 int main() {
+    const char* s = "hello world";
+    std::cout << faStr3(s) << std::endl;
     return 0;
 }
