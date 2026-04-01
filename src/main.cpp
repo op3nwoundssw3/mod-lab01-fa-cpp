afda947df25a9cda344534e842805cd686ceeabd
op3nwoundssw3 <op3nwoundss@mail.ru>
2026-04-01 23:59:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main() {
     const char* str = "Hello world123 raz dva vanek9423423";
     std::cout << faStr1(str) << "\n";
     std::cout << faStr2(str) << "\n";
+    
     std::cout << faStr3(str) << "\n";
     return 0;
 }
